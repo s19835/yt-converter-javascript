03a4a4395945b9462529613f1673df816288beb8
s19835 <s19835@sci.pdn.ac.lk>
2024-07-20 23:47:29 +0530

    separator string

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 #!/usr/bin/env node
-import { program } from "commander";
+import { Command } from "commander";
 
-console.log("Hello World");
+//create new action handler for commands
+const program = new Command();
 
 program
-    .option('--first')
-    .option('-s --separator <char>');
+    .name('string-util')
+    .description('CLI to some JavaScript string utilities')
+    .version('0.8.0');
+
+program
+    .command('split')
+    .description('Split a string into substrings and display as an array')
+    .argument('<string>', 'string to split')
+    .option('--first', 'display the first substring')
+    .option('-s, --separator <char>', 'separator charactor', ',')
+    .action((str, options) => {
+        const limit = options.first ? 1 : undefined;
+        str ? console.log(str.split(options.separator, limit)) : console.log('Provide valid input string');
+    });
 
 program.parse();
-
-const options = program.opts();
-const limit = options.first ? 1 : undefined;
-program.args[0] ? console.log(program.args[0].split(options.separator, limit)): console.log('provide a valid input');
