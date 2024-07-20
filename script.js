d78f1f2a291ca60541ee4544db7580dd4c42680c
s19835 <s19835@sci.pdn.ac.lk>
2024-07-20 22:49:38 +0530

    basic demo

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 #!/usr/bin/env node
+import { program } from "commander";
+
+console.log("Hello World");
+
+program
+    .option('--first')
+    .option('-s --separator <char>');
+
+program.parse();
+
+const options = program.opts();
+const limit = options.first ? 1 : undefined;
+program.args[0] ? console.log(program.args[0].split(options.separator, limit)): console.log('provide a valid input');
