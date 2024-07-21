70a6ee557f8184fc8b5b189f24fd83c965addb9e
s19835 <s19835@sci.pdn.ac.lk>
2024-07-21 21:54:51 +0530

    download logic with ytdl

--- download-video.js
+++ download-video.js
@@ -1,7 +1,29 @@
-import path from "path";
+import path, { resolve } from "path";
+import fs from 'fs';
 import ytdl from "ytdl-core";
+import { rejects } from "assert";
 
 export async function download(videoUrl, folderName, fileName) {
     const filePath = path.join(folderName, `${fileName}.mp4`);
-    console.log(filePath);
+    
+    if (fs.existsSync(filePath)) throw new Error('File already exsist');
+
+    if (!fs.existsSync(folderName)) fs.mkdir(folderName);
+    
+    const videoStream = ytdl(videoUrl, { quality: 'highest'});
+    const writeStream = fs.createWriteStream(filePath);
+
+    return new Promise((resolve, reject) => {
+        videoStream.pipe(writeStream);
+
+        writeStream.on('finish', () => {
+            console.log('video download to ', filePath);
+            resolve();
+        });
+
+        writeStream.on('error', () => {
+            console.error('Error writing video to disk');
+            reject();
+        });
+    });
 }
