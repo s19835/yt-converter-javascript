d59b69ffc22580894cecc638990158433fae7a6e
s19835 <s19835@sci.pdn.ac.lk>
2024-07-21 21:27:08 +0530

    add yt-converter

--- script.js
+++ script.js
@@ -2,4 +2,21 @@
 import { Command } from "commander";
 import { download } from "./download-video.js";
 
-download('http://hi/hello', 'video', 'file');
+const program = new Command();
+
+program
+    .name('yt-converter')
+    .description('CLI to some JavaScript')
+    .version('0.1.0');
+
+program
+    .command('download <videoUrl>')
+    .description('Download a video from YouTube')
+    .option('-f, --folder <folderName>', 'Output folder name', 'videos')
+    .option('-n, --name <fileName>', 'Output file name', 'video')
+    .action(async (videoUrl, options) => {
+        const { folder: folderName, name: fileName } = options;
+        download(videoUrl, folderName, fileName);
+    });
+
+program.parse();
